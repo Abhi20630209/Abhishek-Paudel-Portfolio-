ac98456433248111716509679ec8e1648b41166c
Abhishek Paudel <abhishekpwdl7@gmail.com>
2026-01-31 13:58:04 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// BURGER MENU TOGGLE
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    burger.classList.toggle('toggle'); // optional animation if you add CSS for .toggle
+});
